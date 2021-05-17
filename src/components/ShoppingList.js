d9766b589b362b9f717cf19c9cb4b85f8c282f53
sydney <sydneychase5841@gmail.com>
2021-05-16 21:58:15 -0400

    Done with Assignment

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  
+  function handleCategoryChange(event) {
+   
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+    return item.category === selectedCategory;
+  });
+  
   return (
     <div className="ShoppingList">
       <div className="Filter">
